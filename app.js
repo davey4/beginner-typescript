7f974955837602e9f5e7bc69385ebb86f0e42f35
David Martel <daveymartel@gmail.com>
2021-01-27 10:50:06 -0500

    enum

--- app.js
+++ app.js
@@ -11,16 +11,36 @@ var result = add(number1, number2);
 //   name: string;
 //   age: number;
 // }
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //tuple
+// } = {
+//   name: "Max",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+// giving up all tsc benefits with any
+var favoriteActivities;
+favoriteActivities = ["Sports", 1];
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Max",
     age: 30,
     hobbies: ["Sports", "Cooking"],
+    role: Role.ADMIN,
 };
-// giving up all tsc benefits with any
-var favoriteActivities;
-favoriteActivities = ["Sports", 1];
 console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-}
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase());
+// }
--- app.ts
+++ app.ts
@@ -14,21 +14,38 @@ const result = add(number1, number2);
 //   name: string;
 //   age: number;
 // }
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //tuple
-} = {
-  name: "Max",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
-};
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //tuple
+// } = {
+//   name: "Max",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
 
 // giving up all tsc benefits with any
 let favoriteActivities: any[];
 favoriteActivities = ["Sports", 1];
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: "Max",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
 
 console.log(person);
 
